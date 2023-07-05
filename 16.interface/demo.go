6a11539b3b13a8cc99f06f748d96b52442a8dc9a
zhuyuanhao <631549374@qq.com>
2023-07-05 20:15:31 +0800

    7/5 笔记本

--- 16.interface/demo.go
+++ 16.interface/demo.go
@@ -5,6 +5,7 @@ import "fmt"
 // 接口的声明
 type Mobiler interface {
 	Call(i int)
+	Send(string2 string) bool
 }
 type Huawei struct {
 	Name string
@@ -14,16 +15,29 @@ func (h Huawei) Call(mo int) {
 	fmt.Println(h.Name, " call phone", mo)
 }
 
+func (h Huawei) Send(mo string) bool {
+	fmt.Println(mo)
+	return true
+}
+func (h Huawei) playGame() {
+	fmt.Println("play game")
+}
+
 func main() {
 	var m Mobiler
-	var h = Huawei{
-		Name: "p40",
-	}
+	//var h = &Huawei{"p40"}
+	m = &Huawei{"p40"} //1.用接口方式调用----对象实例赋值给接口
+	//h.playGame()
 	//h.Call(110) 实例化调用
-	//用接口调用
-	m = h
-	m.Call(12222)
 
+	//1.用接口方式调用----对象实例赋值给接口
+	//m = h
+	m.Call(122222222)
+	m.Send("546")
+	//2.把接口赋值给另一个接口
+	var m2 Mobiler = &Huawei{"p60"}
+	m2 = m
+	m2.Send("Two-接口")
 }
 
 /*
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module Go-Exercise
 
 go 1.20
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/twindagger/httpsig v1.2.0 // indirect
+	gopkg.in/twindagger/httpsig.v1 v1.2.0 // indirect
+)
