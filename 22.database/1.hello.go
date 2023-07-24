61f9b6734b2780581b8d7135dc91f5e32b59f0ff
zhuyuanhao <631549374@qq.com>
2023-07-25 00:36:12 +0800

    home 7/23

--- 22.database/1.hello.go
+++ 22.database/1.hello.go
@@ -2,30 +2,21 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
-//	func init() {
-//		var db *sql.DB
-//		var dbErr error
-//		//DSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s", root, PASS_WORD, HOST, PORT, DATABASE, CHARSET)
-//		db, dbErr = sql.Open("mysql", "root:123456@tcp(10.1.1.11:3306)/test")
-//		if dbErr != nil {
-//			log.Fatal(dbErr.Error())
-//		}
-//		if dbErr = db.Ping(); nil != dbErr {
-//			log.Fatal("数据库链接失败: " + dbErr.Error())
-//		}
-//		db.SetMaxIdleConns(200)
-//		db.SetMaxIdleConns(10)
-//	}
-func main() {
-	//init()
+var db *sql.DB
+var dbErr error
+
+func init() {
 	//"用户名:密码@[连接方式](主机名:端口号)/数据库名"
-	var db *sql.DB
-	var dbErr error
-	db, dbErr = sql.Open("mysql", "root:@tcp(10.1.1.11:3306)/test") // 设置连接数据库的参数
+
+	//笔记本的数据库
+	//db, dbErr = sql.Open("mysql", "root:123456@tcp(10.1.1.11:3306)/test") // 设置连接数据库的参数
+	//家里的数据库
+	db, dbErr = sql.Open("mysql", "root:123456@tcp(192.168.1.10:3306)/test?charset=utf8") // 设置连接数据库的参数
 	if dbErr != nil {
 		log.Fatal(dbErr.Error())
 	}
@@ -34,5 +25,22 @@ func main() {
 	}
 	db.SetMaxIdleConns(200)
 	db.SetMaxIdleConns(10)
+}
+
+func inserData() {
+	//res, err := db.Exec("INSERT INTO user (name,email)VALUES (?,?)", "map", "666@qq.com")
+	//prepare
+	pre, err := db.Prepare("INSERT INTO user (name,email)VALUES (?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, _ := pre.Exec("map", "666@qq.com")
+	id, _ := res.LastInsertId()  //id号
+	row, _ := res.RowsAffected() //修改到的行的数量
+	fmt.Println(id, row)
+}
+func main() {
+	defer db.Close()
+	inserData()
 
 }
