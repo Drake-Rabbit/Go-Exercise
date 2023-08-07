f6e966783ed8f7ce5af003dbb37b64737262f9ee
zhuyuanhao <631549374@qq.com>
2023-08-08 00:36:54 +0800

    home 7/8

--- 25.verification-code/1.demo.go
+++ 25.verification-code/1.demo.go
@@ -5,6 +5,7 @@ import (
 	"github.com/mojocn/base64Captcha"
 	"html/template"
 	"image/color"
+	"log"
 	"net/http"
 )
 
@@ -12,12 +13,12 @@ func main() {
 
 	http.HandleFunc("/index", index)
 	//
-
+	//
 	//api for create captcha
 	http.HandleFunc("/api/getCaptcha", generateCaptchaHandler)
 
 	//api for verify captcha
-	http.HandleFunc("/api/verifyCaptcha", captchaVerifyHandle)
+	http.HandleFunc("/api/verifyCaptcha", verifyCaptchaHandler)
 
 	//fmt.Println("Server is at :8777")
 	//if err := http.ListenAndServe(":8777", nil); err != nil {
@@ -26,8 +27,26 @@ func main() {
 	http.ListenAndServe(":8777", nil)
 }
 
+func verifyCaptchaHandler(writer http.ResponseWriter, request *http.Request) {
+	captcha := request.PostFormValue("captcha")
+	captcha_id := request.PostFormValue("captcha_id")
+
+	body := map[string]interface{}{"code": 0, "msg": "failed"}
+	if store.Verify(captcha_id, captcha, true) {
+		body = map[string]interface{}{"code": 1, "msg": "ok"}
+	}
+	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(writer).Encode(body)
+
+	//fmt.Println(captcha, captcha_id)
+}
+
 func index(writer http.ResponseWriter, request *http.Request) {
-	t, _ := template.ParseFiles("captview/index.html")
+	t, err := template.ParseFiles("captview/index.html")
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
 	t.Execute(writer, nil)
 }
 
@@ -38,24 +57,18 @@ func captchaVerifyHandle(writer http.ResponseWriter, request *http.Request) {
 var store = base64Captcha.DefaultMemStore
 
 func generateCaptchaHandler(w http.ResponseWriter, r *http.Request) {
-
-	drviceString := base64Captcha.DriverString{
+	driverString := base64Captcha.DriverString{
 		Height:          60,
 		Width:           200,
 		NoiseCount:      0,
 		ShowLineOptions: 2 | 4,
 		Length:          4,
-		Source:          "123456789ABCDEFGHIJKMNOPQRSTUVWXYZ",
-		BgColor: &color.RGBA{
-			R: 5,
-			G: 10,
-			B: 23,
-			A: 10,
-		},
+		Source:          "1234567890qwertyuioplkjhgfdsazxcvbnm",
+		BgColor:         &color.RGBA{R: 3, G: 102, B: 214, A: 125},
+
 		Fonts: []string{"wqy-microhei.ttc"},
 	}
-
-	var driver base64Captcha.Driver = drviceString.ConvertFonts()
+	var driver base64Captcha.Driver = driverString.ConvertFonts()
 	c := base64Captcha.NewCaptcha(driver, store)
 	id, b64s, err := c.Generate()
 	body := map[string]interface{}{"code": 1, "data": b64s, "captchaId": id, "msg": "success"}
